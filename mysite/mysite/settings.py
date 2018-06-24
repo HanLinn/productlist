c62c8aaa10bc2f3f3baa188354ab7b4e900143a8
Han Linn <hanlinnaung87@gmail.com>
2018-06-24 12:35:37 +0630

    First Commit to GH

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uag@6s%hb9+4k(*urx2+fzoaxu+p#@$l6ch_dyzj5^1)s_oad7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.118"]
 
 
 # Application definition
--- mysite/pricelist/forms.py
+++ mysite/pricelist/forms.py
@@ -7,3 +7,10 @@ class detail(ModelForm):
     class Meta:
         model = Pricelist
         fields = ['name', 'price', 'category']
+        widgets = { 
+            'name': forms.TextInput(attrs={'placeholder': 'Name','class': "mm",'class':"form-control"}),
+            'price': forms.NumberInput(attrs={'placeholder': 'Price','class': "mm",'class':"form-control"}),
+            'category': forms.Select(attrs={'placeholder': 'category',"class": "mm",'class':"form-control"})
+        }
+
+        
